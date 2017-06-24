f650dd4ea57aa94ccc1d7ea50b937dba2884a0a1
Sudhir Shrestha <sudhirshrestha@live.com>
2017-06-24 10:12:36 +0545

    Tilemap setup

---
+++ src/tile-map/tile-map-row.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class TileMapRow extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <tr>
+                {
+                    this.props.row.map((d, index) => {
+                        return (
+                            <td key={index} className={`tile-map-row block-${d}`}>
+                            </td>
+                        )
+                    })
+                }
+            </tr>)
+    }
+}
