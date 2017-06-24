c1f1aa774eee6bc460a2a75f99d043c1ce3f90bb
Prakash Sharma <prakashpoudel@lftechnology.com>
2017-06-24 12:16:34 +0545

    detect some noise

--- src/App.js
+++ src/App.js
@@ -20,12 +20,35 @@ getAudioPermission.then(function (stream) {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   var audioContext = new AudioContext();
 
+  var inputPoint = audioContext.createGain()
+
   // Create an AudioNode from the stream
   var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
+  mediaStreamSource.connect(inputPoint);
+
+  var analyserNode = audioContext.createAnalyser();
+  analyserNode.fftSize = 2048;
+  inputPoint.connect(analyserNode);
+
+  setInterval(function () {
+    var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
+    analyserNode.getByteFrequencyData(freqByteData);
+
+    var sum = 0;
+    for (var i = 0; i < freqByteData.length; i++) {
+      sum += freqByteData[i];
+    }
+
+    let voiceLevel = sum / freqByteData.length;
+    if(voiceLevel>30){
+      console.log(voiceLevel);
+    }
+  }, 200);
+
   // Connect it to destination to hear yourself
   // or any other node for processing!
-  mediaStreamSource.connect(audioContext.destination);
+  // mediaStreamSource.connect(audioContext.destination);
 
 
 }).catch(function (err) {
