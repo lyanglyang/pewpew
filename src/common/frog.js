1807a79ab0e1a6576338db564ca2b3b5b2673d67
joshianuj <joshi.anujo7@gmail.com>
2017-06-25 15:15:57 +0200

    on key up event

--- src/common/frog.js
+++ src/common/frog.js
@@ -48,7 +48,7 @@ export default class Frog extends React.Component {
   }
 
   setKeyBindings() {
-    document.onkeydown = (e) => {
+    document.onkeyup = (e) => {
       e = e || window.event;
       let playerPosition = Object.assign({}, this.props.player.position);
       switch (e.which || e.keyCode) {
@@ -75,10 +75,9 @@ export default class Frog extends React.Component {
         case 32:
           this.pewpew();
           return;
-          break;
+
         default:
           return;
-          break;
       }
       this.props.setPlayerPosition(playerPosition);
     }
